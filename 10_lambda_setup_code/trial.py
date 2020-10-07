87486d0963143ff5c8bfa59c4fb844cd116b7aee
Wilson Huang <yichenghuang980@gmail.com>
2020-10-07 18:23:05 +0000

    rearrange

---
+++ 10_lambda_setup_code/trial.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape(url):
+    html_content = requests.get(url)
+  
+    html_content.raise_for_status()
+    
+    soup = BeautifulSoup(html_content.text, "lxml")
+    print(soup.title.text)
+    
+    btc_table = soup.find('table', attrs = {'class': 'styled-table full-size-table'})
+    btc_data = btc_table.tbody.find_all("tr")
+    header = (btc_data[0].find_all("th"))
+    
+    headings = []
+    for td in btc_data[0].find_all("th"):
+        # remove any newlines and extra spaces
+        headings.append(td.text.replace('\n', ' ').strip())
+        print(headings)
+        pass
+    
+    data = {}
+    # Get all the rows
+    table_data = []
+    for tr in btc_table.tbody.find_all("tr"): # find all tr's from table's tbody
+        t_row = {}
+        # t_row = {'Date': '', 'Open': '', 'High': '', 'Close': '', 'Volume': '', 'Market Cap': ''}
+
+        # find all td's(6) in tr and zip it with t_header
+        for td, th in zip(tr.find_all("td"), headings): 
+            t_row[th] = td.text.replace('\n', '').strip().replace('$\u202f','')
+        table_data.append(t_row)
+
+    print(table_data)
